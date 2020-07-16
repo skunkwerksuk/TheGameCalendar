c789d97e08f540b7971aeee356cd3d1d1f3c14fe
Alexander Youngman <alexander.youngman@nhs.net>
2020-07-16 23:30:30 +0100

    Removed excess style from side nav

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -21,7 +21,7 @@ class SidePanel extends React.Component {
     return <div className="side-panel" id="sidePanel">
       <h1 className="page-title" onClick={refresh}>GAME<br/>CAL</h1>
       <h2 className="sub-title m-b-40">Upcoming Video Game Releases</h2>
-      <nav className="m-b-30">
+      <nav className="">
         {this.props.yearBoundary === -4 ? <button className="month-nav disabled" disabled>&#60;&#60;</button> : <Link className="month-nav" to={monthNames[prevMonthId]} onClick={this.props.prevMonth}>&#60;&#60;</Link>}
         <div>
           <h3 className="month-name">{this.props.month}</h3>
@@ -37,28 +37,30 @@ class SidePanel extends React.Component {
       </div>
       
       <div className="filters">
-        <label>Platforms:</label>
-        <div className="checkbox-wrapper">
-          <input onChange={this.props.setFilters} type="checkbox" name="xboxOne" value="Xbox One" id="xboxOne" className="filter-checkbox" />
-          <label htmlFor="xboxOne" id="xboxOneCheckbox">Xbox One</label>
+        <label>Filter</label>
+        <div className="icons m-b-5">
+          <div className="checkbox-wrapper">
+            <input onChange={this.props.setFilters} type="checkbox" name="xboxOne" value="Xbox One" id="xboxOne" className="filter-checkbox" />
+            <label htmlFor="xboxOne" id="xboxOneCheckbox"></label>
+          </div>
+          <div className="checkbox-wrapper">
+            <input onChange={this.props.setFilters} type="checkbox" name="ps4" value="PlayStation 4" id="ps4" className="filter-checkbox" />
+            <label htmlFor="ps4" id="ps4Checkbox"></label>
+          </div>
+          <div className="checkbox-wrapper">
+            <input onChange={this.props.setFilters} type="checkbox" name="nintendoSwitch" value="Nintendo Switch" id="nintendoSwitch" className="filter-checkbox" />
+            <label htmlFor="nintendoSwitch" id="nintendoSwitchCheckbox"></label>
+          </div>
+          <div className="checkbox-wrapper">
+            <input onChange={this.props.setFilters} type="checkbox" name="pc" value="PC (Microsoft Windows)" id="pc" className="filter-checkbox" />
+            <label htmlFor="pc" id="pcCheckbox"></label>
+          </div>
+          <div className="checkbox-wrapper">
+            <input onChange={this.props.setFilters} type="checkbox" name="stadia" value="Google Stadia" id="stadia" className="filter-checkbox" />
+            <label htmlFor="stadia" id="stadiaCheckbox"></label>
+          </div>
         </div>
-        <div className="checkbox-wrapper">
-          <input onChange={this.props.setFilters} type="checkbox" name="ps4" value="PlayStation 4" id="ps4" className="filter-checkbox" />
-          <label htmlFor="ps4" id="ps4Checkbox">PlayStation 4</label>
-        </div>
-        <div className="checkbox-wrapper">
-          <input onChange={this.props.setFilters} type="checkbox" name="nintendoSwitch" value="Nintendo Switch" id="nintendoSwitch" className="filter-checkbox" />
-          <label htmlFor="nintendoSwitch" id="nintendoSwitchCheckbox">Nintendo Switch</label>
-        </div>
-        <div className="checkbox-wrapper">
-          <input onChange={this.props.setFilters} type="checkbox" name="pc" value="PC (Microsoft Windows)" id="pc" className="filter-checkbox" />
-          <label htmlFor="pc" id="pcCheckbox">PC</label>
-        </div>
-        <div className="checkbox-wrapper">
-          <input onChange={this.props.setFilters} type="checkbox" name="stadia" value="Google Stadia" id="stadia" className="filter-checkbox" />
-          <label htmlFor="stadia" id="stadiaCheckbox">Google Stadia</label>
-        </div>
-        <button className="button primary" onClick={this.props.clearFilters}>Clear</button>
+        <button className="button primary" onClick={this.props.clearFilters}>Clear Filters</button>
       </div>
       <div className="signature">
         <p>Developed by: <a href="https://github.com/skunkwerksuk">@skunkwerksuk</a></p>
